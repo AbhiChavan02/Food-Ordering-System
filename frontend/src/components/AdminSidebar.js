c8de60f9f2029a254ee80cce96fce7c230f001ad
AbhiChavan02 <abhijeetchavan212002@gmail.com>
2026-04-03 16:07:03 +0530

    Django and React project setup with stage 1

---
+++ frontend/src/components/AdminSidebar.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+import { FaChevronDown, FaChevronUp, FaEdit, FaThLarge, FaUsers } from "react-icons/fa";
+
+const AdminSidebar = () => {
+  const [openMenus, setOpenMenus] = useState({
+    category: false,
+    food: false,
+    orders: false,
+  });
+
+  const toggleMenu = (menu) => {
+    setOpenMenus((prev) => ({ ...prev, [menu]: !prev[menu] }));
+  };
+
+  return (
+    <div className="bg-dark text-white sidebar">
+      <div className="text-center p-3 border-bottom">
+        <img
+          src="/images/admin.png"
+          className="img-fluid rounded-circle mb-2"
+          width="70"
+          alt="Admin Img"
+        />
+        <h6 className="mb-0">Admin</h6>
+      </div>
+
+      <div className="list-group list-group-flush">
+        <Link className="list-group-item list-group-item-action bg-dark text-white">
+          <FaThLarge /> Dashboard
+        </Link>
+
+        <div className="list-group list-group-flush">
+          <Link className="list-group-item list-group-item-action bg-dark text-white">
+            <FaUsers /> Reg Users
+          </Link>
+        </div>
+
+        <button
+          onClick={() => toggleMenu("category")}
+          className="list-group-item list-group-item-action bg-dark text-white border-0"
+        >
+          <FaEdit /> Food Category {openMenus.category ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
+
+        {openMenus.category && (
+          <div className="ps-4">
+            <Link className="list-group-item list-group-item-action bg-dark text-white border-0">
+              Add Category
+            </Link>
+
+            <Link className="list-group-item list-group-item-action bg-dark text-white border-0">
+              Manage Category
+            </Link>
+          </div>
+        )}
+
+        <button
+          onClick={() => toggleMenu("food")}
+          className="list-group-item list-group-item-action bg-dark text-white border-0"
+        >
+          <FaEdit /> Food Item {openMenus.food ? <FaChevronUp /> : <FaChevronDown />}
+        </button>
+
+        {openMenus.food && (
+          <div className="ps-4">
+            <Link className="list-group-item list-group-item-action bg-dark text-white border-0">
+              Add Food
+            </Link>
+
+            <Link className="list-group-item list-group-item-action bg-dark text-white border-0">
+              Manage Food
+            </Link>
+          </div>
+        )}
+
+        <div className="list-group list-group-flush">
+          <Link className="list-group-item list-group-item-action bg-dark text-white">
+            <FaThLarge /> Search
+          </Link>
+        </div>
+
+        <div className="list-group list-group-flush">
+          <Link className="list-group-item list-group-item-action bg-dark text-white">
+            <FaThLarge /> Manage Reviews
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminSidebar;
---
+++ frontend/src/pages/AdminDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Adminlayout from '../components/Adminlayout'
+
+const AdminDashboard = () => {
+  return (
+        <Adminlayout>
+          <div>
+            <h2 className='text-center text-primary'>Admin Dashboard</h2>
+          </div>
+        </Adminlayout>
+  )
+}
+
+export default AdminDashboard
